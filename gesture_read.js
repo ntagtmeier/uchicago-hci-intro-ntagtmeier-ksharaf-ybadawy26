ef8b5e3235e7ac503dd85b1b2f024be53b4edecc
Ned Tagtmeier <ntagtmeier@DESKTOP-D717DR0>
2024-10-22 11:55:23 -0500

    combined new way of displaying results with karim's changes

--- gesture_read.js
+++ gesture_read.js
@@ -64,59 +64,8 @@ export const gesture_read = (callback) => {
   });
 
   const finish = () => {
-    hammertime.off('swipe');
-    console.log('Gesture Sequence Completed:', results); // Log the final results
-    hammertime.on('tap', () => {
-      callback();
-      hammertime.destroy();
-    });
-  };
-};
-export const gesture_read = (callback) => {
-  var hammertime = new Hammer(window);
-  var results = [];
-  hammertime.get('swipe').set({ direction: Hammer.DIRECTION_ALL });
-  hammertime.get('pinch').set({ enable: true });
-
-  hammertime.on('swipeup', function () {
-    results.push(0);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  hammertime.on('swiperight', function () {
-    results.push(1);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  hammertime.on('swipedown', function () {
-    results.push(2);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  hammertime.on('swipeleft', function () {
-    results.push(3);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  hammertime.on('pinchin', function () {
-    results.push(4);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  hammertime.on('pinchout', function () {
-    results.push(5);
-    if (results.length == 5) {
-      finish();
-    }
-  });
-  const finish = () => {
     hammertime.destroy();
-    console.log(results);
+    console.log('Gesture Sequence Completed:', results); // Log the final results
     var results_translated = [];
     for (let result of results) {
       switch (result) {
--- morse_read.js
+++ morse_read.js
@@ -21,34 +21,8 @@ export const morse_read = (callback) => {
   });
 
   const finish = () => {
-    hammertime.off('tap');
-    hammertime.off('press');
-    console.log('Morse Code Sequence Completed:', results); // Log the final results
-    hammertime.on('tap', () => {
-      hammertime.destroy();
-      callback();
-    });
-  };
-};
-export const morse_read = (callback) => {
-  var hammertime = new Hammer(window);
-  var results = [];
-
-  hammertime.on('tap', function () {
-    results.push(0);
-    if (results.length == 15) {
-      finish();
-    }
-  });
-  hammertime.on('press', function () {
-    results.push(1);
-    if (results.length == 15) {
-      finish();
-    }
-  });
-  const finish = () => {
     hammertime.destroy();
-    console.log(results);
+    console.log('Morse Code Sequence Completed:', results); // Log the final results
     var results_translated = [];
     var i = 0;
     while (i < results.length) {
